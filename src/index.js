fc7c9cab87facfee8a2f369daa65aa8f6df428b4
Angela Branaes <angela.branaes@gmail.com>
2017-11-25 11:34:02 +0000

    Make brand header smaller on small screen devices

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/index.css';
-import App from './App';
+import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
