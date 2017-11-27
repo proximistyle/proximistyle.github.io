c4ee7bf7dd65be084c8ae441f9635598dd9ebda7
Angela Branaes <angela.branaes@gmail.com>
2017-11-27 00:23:13 +0000

    Add background image as hidden image to speed up loading priority

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import { PageHeader, Grid, Row, Col } from 'react-bootstrap';
 import './styles/Header.css'
 import Signup from './Signup.jsx'
+import HeaderImage from './images/header.jpg'
 
 export default class Header extends Component {
 
   render() {
+    const imgStyle = {display: "none"};
     return (
       <PageHeader className="hero">
+        <img src={HeaderImage} style={imgStyle} />
         <Grid>
           <Row>
             <Col className="white-square" md={6} mdOffset={3}>
